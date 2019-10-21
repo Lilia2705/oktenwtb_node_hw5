9261c508a0b451d407ccee540ba67cdbd915ea7f
Lilia <52850127+Lilia2705@users.noreply.github.com>
2019-10-21 21:11:23 +0200

    Update houseRouter.js

--- router/houseRouter/houseRouter.js
+++ router/houseRouter/houseRouter.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 let {house} = require('../../controllers');
 let {houseMiddleware} = require('../../middleware');
 
-router.post('/houses', houseMiddleware.checkHouseValidMiddleware, house.createHouse);
+router.post('/', houseMiddleware.checkHouseValidMiddleware, house.createHouse);
 
 router.get('/:house_id', houseMiddleware.isHousePresent, house.getHouse);
 router.get ('/', house.findAllHouses);
